1d3db83e9339a5ed698c39c1a1d9eecf657eeecd
Bryan Veloso <bryan@avalonstar.com>
2017-08-14 16:30:06 -0700

    Landing page, an actual landing page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import './App.css';
+import logomark from './logomark.svg';
+import logotype from './logotype.svg';
+
+const App = () =>
+  <div className="app">
+    <div className="container">
+      <a className="logo">
+        <img src={logomark} className="logomark" alt="synthform.tv" />
+        <img src={logotype} className="logotype" alt="synthform.tv" />
+      </a>
+    </div>
+
+    <main />
+  </div>;
 
 export default App;
