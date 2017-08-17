80b316866fddc484eb4d247ff285a9f06893f865
Bryan Veloso <bryan@avalonstar.com>
2017-08-16 22:42:31 -0700

    Reeeeeduxxxxxx.

---
+++ src/helpers/api.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import moment from 'moment';
+
+import { ref } from 'configurations/constants';
+
+export function listenToMessages(channel, cb, errorCb) {
+  return ref.child(`messages/${channel}/events`).limitToLast(40).on(
+    'value',
+    snapshot => {
+      const messages = snapshot.val() || [];
+      cb(messages);
+    },
+    errorCb
+  );
+}
---
+++ src/modules/messages.js
@@ -0,0 +1,79 @@
+import { addListener } from 'modules/listeners';
+import { fromJS } from 'immutable';
+import { listenToMessages } from 'helpers/api';
+
+const SETTING_MESSAGE_LISTENER = 'SETTING_MESSAGE_LISTENER';
+const SETTING_MESSAGE_LISTENER_ERROR = 'SETTING_MESSAGE_LISTENER_ERROR';
+const SETTING_MESSAGE_LISTENER_SUCCESS = 'SETTING_MESSAGE_LISTENER_SUCCESS';
+
+function settingMessageListener() {
+  return {
+    type: SETTING_MESSAGE_LISTENER
+  };
+}
+
+function settingMessageListenerError(error) {
+  console.warn(error);
+  return {
+    type: SETTING_MESSAGE_LISTENER_ERROR,
+    error: 'Error fetching messages.'
+  };
+}
+
+function settingMessageListenerSuccess(channel, messages, lastUpdated) {
+  return {
+    type: SETTING_MESSAGE_LISTENER_SUCCESS,
+    channel,
+    messages,
+    lastUpdated
+  };
+}
+
+export function setAndHandleMessageListener(channel) {
+  return function(dispatch, getState) {
+    if (getState().listeners.messages === true) {
+      return;
+    }
+
+    dispatch(addListener('messages'));
+    dispatch(settingMessageListener());
+
+    listenToMessages(
+      channel,
+      messages => {
+        dispatch(settingMessageListenerSuccess(channel, messages, Date.now()));
+      },
+      error => dispatch(settingMessageListenerError(error))
+    );
+  };
+}
+
+const initialState = fromJS({
+  isFetching: false,
+  error: ''
+});
+
+export default function messages(state = initialState, action) {
+  switch (action.type) {
+    case SETTING_MESSAGE_LISTENER:
+      return state.merge({
+        isFetching: true
+      });
+    case SETTING_MESSAGE_LISTENER_ERROR:
+      return state.merge({
+        isFetching: false,
+        error: action.error
+      });
+    case SETTING_MESSAGE_LISTENER_SUCCESS:
+      return state.merge({
+        isFetching: false,
+        error: '',
+        [action.channel]: {
+          lastUpdated: action.lastUpdated,
+          messages: action.messages
+        }
+      });
+    default:
+      return state;
+  }
+}
