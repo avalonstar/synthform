621bc0d9a5f7af1a8133a71a1a39e12c4cf58418
Bryan Veloso <bryan@avalonstar.com>
2017-08-19 20:40:02 -0700

    Split out the API files.

---
+++ src/helpers/api/events.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+
+import { ref } from 'configurations/constants';
+
+export function listenToEvents(channel, limit, cb, errorCb) {
+  return ref.child(`events/${channel}`).limitToLast(limit).on(
+    'value',
+    snapshot => {
+      const events = snapshot.val();
+      const payload = _.uniqBy(
+        _.orderBy(events, 'timestamp', 'desc'),
+        'timestamp'
+      );
+      cb(payload);
+    },
+    errorCb
+  );
+}
