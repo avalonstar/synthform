2d547d3d4a61d3a3fbacc1f1ceb148ab8c550dc7
Bryan Veloso <bryan@avalonstar.com>
2017-08-31 01:17:54 -0700

    Add Camera component basics.

---
+++ src/components/Cameras/Cameras.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Cameras.css';
+
+export function MainCamera() {
+  return (
+    <div className="cam cam-main">
+      <div />
+    </div>
+  );
+}
+
+export function SecondaryCamera() {
+  return (
+    <div className="cam cam-secondary">
+      <div />
+    </div>
+  );
+}
