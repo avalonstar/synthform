2f740e0a950b1fdf6a31fb4b6464dd2b85b06044
Bryan Veloso <bryan@avalonstar.com>
2017-10-02 05:16:56 -0700

    Remove `firebase` and `firebase-admin`.

--- src/configurations/constants.js
+++ src/configurations/constants.js
@@ -1,13 +1,3 @@
-import firebase from 'firebase';
-
-firebase.initializeApp({
-  apiKey: 'AIzaSyAXPYxFZanf33ZDgc5nI-JADDJqJWHANec',
-  authDomain: 'synthform.firebaseapp.com',
-  databaseURL: 'https://synthform.firebaseio.com',
-  storageBucket: 'project-2060008184115091556.appspot.com'
-});
-export const ref = firebase.database().ref();
-
 export const tmiIdentity = {
   username: 'synthformer',
   password: 'oauth:sf2bdj907ubkgumu0xej9w91fns0fh'
--- src/helpers/api/index.js
+++
@@ -1,1 +0,0 @@
-export { default as listenToEvents } from './events';
