20436744e751ccde94cae8be52b95b8c6964c288
Bryan Veloso <bryan@avalonstar.com>
2017-11-15 11:45:59 -0800

    Minus Twitter.

--- src/components/Labels/SocialMedia.js
+++ src/components/Labels/SocialMedia.js
@@ -3,20 +3,17 @@ import { ChevronRight, Twitter, Tv } from 'react-feather';
 
 import './SocialMedia.css';
 
-const SocialMedia = () =>
+const SocialMedia = () => (
   <div className="sm">
     <div className="sm-node sm-cap">
       <ChevronRight size={18} color="#02fa7b" />
       {'follow'}
     </div>
-    <div className="sm-node sm-twitter">
-      <Twitter size={18} color="#1da1f2" />
-      {'avalonstar'}
-    </div>
     <div className="sm-node sm-youtube">
       <Tv size={18} color="#cd201f" />
       {'avalonstar'}
     </div>
-  </div>;
+  </div>
+);
 
 export default SocialMedia;
