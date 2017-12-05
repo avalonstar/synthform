a187ddc3fbe8fee9d4a98725ebdc00a3eef7bf55
Bryan Veloso <bryan@avalonstar.com>
2017-12-05 00:17:26 -0800

    Convert CounterItem to styled-components.

--- src/components/Emotes/CounterItem.js
+++ src/components/Emotes/CounterItem.js
@@ -3,8 +3,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import numeral from 'numeral';
-
-import './CounterItem.css';
+import styled from 'styled-components';
 
 const propTypes = {
   id: PropTypes.string.isRequired,
@@ -12,18 +11,42 @@ const propTypes = {
   count: PropTypes.number.isRequired
 };
 
+const Item = styled.li`
+  display: flex;
+  align-items: center;
+  padding-top: 8px;
+
+  font-family: 'Gotham SSm A', 'Gotham SSm B';
+
+  img {
+    position: relative;
+    z-index: 10;
+  }
+`;
+
+const Count = styled.span`
+  position: relative;
+  padding: 6px 12px 6px 26px;
+  margin-left: -20px;
+
+  background: rgba(0, 0, 0, 0.6);
+  box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.1);
+  border-radius: 4px;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 700;
+`;
+
 class Counter extends Component {
   render() {
     return (
-      <li className="eci">
+      <Item>
         <img
           src={`//static-cdn.jtvnw.net/emoticons/v1/${this.props.id}/2.0`}
           alt={this.props.code}
         />
-        <span className="eci-count">
-          {numeral(this.props.count).format('0,0')}
-        </span>
-      </li>
+        <Count>{numeral(this.props.count).format('0,0')}</Count>
+      </Item>
     );
   }
 }
