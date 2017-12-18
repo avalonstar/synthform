b0a54dee74fa53630641047aa067919e9133f4e8
Bryan Veloso <bryan@avalonstar.com>
2017-12-18 11:51:02 -0800

    Goal and PB displays (WIP).

--- src/components/Goals/SubPoints.js
+++ src/components/Goals/SubPoints.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Motion, spring } from 'react-motion';
+import styled from 'styled-components';
 
 import { subpointFetch } from 'actions/subscriptions';
 import * as selectors from 'selectors';
@@ -23,14 +24,16 @@ const labelPropTypes = {
   goal: PropTypes.number.isRequired
 };
 
-function Indicator(props) {
+const getWidth = (span, end) => span / end * 100;
+
+function Goal({ progress }) {
   return (
-    <Motion defaultStyle={{ x: 0 }} style={{ x: spring(props.progress) }}>
+    <Motion defaultStyle={{ x: 0 }} style={{ x: spring(progress) }}>
       {({ x }) => (
         <div className="spg-bar">
           <div
             className="spg-indicator"
-            data-progress={props.progress}
+            data-progress={progress}
             style={{ width: `${x}%` }}
           />
         </div>
@@ -47,7 +50,7 @@ function Label(props) {
     >
       {({ points }) => (
         <div className="spg-label">
-          <span className="spg-title">SP</span>
+          <span className="spg-title">2017 GOAL</span>
           <div className="spg-progress">
             <span className="spg-points">{Math.round(points)}</span>
             <span className="spg-goal">/{props.goal}</span>
@@ -58,11 +61,28 @@ function Label(props) {
   );
 }
 
+function PB({ points, progress }) {
+  return (
+    <Motion defaultStyle={{ x: 0 }} style={{ x: spring(progress) }}>
+      {({ x }) => (
+        <div
+          className="spg-pb-indicator"
+          data-progress={progress}
+          style={{ width: `${x}%` }}
+        >
+          <span className="spg-pb-text">{points}</span>
+        </div>
+      )}
+    </Motion>
+  );
+}
+
 class SubPointGoal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      goal: 800
+      best: 324,
+      goal: 400
     };
   }
 
@@ -72,20 +92,23 @@ class SubPointGoal extends Component {
 
   render() {
     const points = this.props.subPoints;
-    const { goal } = this.state;
-    const progress = points / goal * 100;
+    const { best, goal } = this.state;
+    const goalWidth = getWidth(points, goal);
+    const bestWidth = getWidth(best, goal);
     return (
       <div className="spg">
-        {Indicator({ progress })}
-        {Label({ points, goal })}
+        {/* <PB points={best} progress={bestWidth} /> */}
+        <Goal progress={goalWidth} />
+        <Label goal={goal} points={points} />
       </div>
     );
   }
 }
 
 SubPointGoal.propTypes = propTypes;
-Indicator.propTypes = indicatorPropTypes;
+Goal.propTypes = indicatorPropTypes;
 Label.propTypes = labelPropTypes;
+PB.propTypes = indicatorPropTypes;
 
 function mapStateToProps(state) {
   return {
