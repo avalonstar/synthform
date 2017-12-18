c0f01ba329cae51a5762fe73826eabc69311daf3
Bryan Veloso <bryan@avalonstar.com>
2017-12-17 16:00:29 -0800

    "Theme" helper for styled-components' sake.

---
+++ src/helpers/foundation.js
@@ -0,0 +1,8 @@
+const foundation = {
+  forza: "'Forza SSm A', 'Forza SSm B'",
+  gotham: "'Gotham SSm A', 'Gotham SSm B'",
+  whitney: "'Whitney SSm A', 'Whitney SSm B'",
+  miedinger: 'Miedinger'
+};
+
+export default foundation;
