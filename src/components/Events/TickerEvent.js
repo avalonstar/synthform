afdfa9a09df688fd33fb5335037420fb1f509704
Bryan Veloso <bryan@avalonstar.com>
2018-01-15 21:47:31 -0800

    Whoops.

--- src/components/Events/TickerEvent.js
+++ src/components/Events/TickerEvent.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
-import { rgba } from 'polished';
 import { PlusSquare } from 'react-feather';
 
 import { getCheermoteURL } from './utils';
