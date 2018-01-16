da90e6f55d957062dfa1743a8f973bcb77e80b4a
Bryan Veloso <bryan@avalonstar.com>
2018-01-15 20:33:04 -0800

    Convert App to styled-components. :lipstick:

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './App.css';
 import logomark from './logomark.svg';
 import logotype from './logotype.svg';
 
-const App = () =>
-  <div className="app">
-    <div className="container">
-      <a className="logo">
-        <img src={logomark} className="logomark" alt="synthform.tv" />
-        <img src={logotype} className="logotype" alt="synthform.tv" />
-      </a>
-    </div>
-  </div>;
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+
+  background: #dee3e8;
+`;
+
+const Logo = styled.a`
+  position: relative;
+`;
+
+const Logomark = styled.img``;
+
+const Logotype = styled.img`
+  position: absolute;
+  right: -20px;
+  bottom: -22px;
+`;
+
+const App = () => (
+  <Container>
+    <Logo>
+      <Logomark src={logomark} alt="synthform.tv" />
+      <Logotype src={logotype} alt="synthform.tv" />
+    </Logo>
+  </Container>
+);
 
 export default App;
