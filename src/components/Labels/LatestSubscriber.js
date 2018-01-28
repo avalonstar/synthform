c809952e7628a1f4cb43e96fe0d20575110ec0e9
Bryan Veloso <bryan@avalonstar.com>
2018-01-27 19:40:29 -0800

    Try to use ellipsis for long names. Also reformat the file.

--- src/components/Labels/LatestSubscriber.js
+++ src/components/Labels/LatestSubscriber.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux';
 import { TransitionMotion, spring } from 'react-motion';
 
 import styled from 'styled-components';
-import { rgba } from 'polished';
+import { ellipsis, rgba } from 'polished';
 import { ChevronRight } from 'react-feather';
 
 import { latestSubscriberFetch } from 'actions/subscriptions';
@@ -67,66 +67,6 @@ const willLeave = () => ({
   opacity: spring(0)
 });
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  padding-right: 8px;
-  overflow: hidden;
-
-  background: linear-gradient(#2c333a, #23292f);
-  border-radius: 4px;
-  box-shadow: 0 1px 3px ${rgba('#090a0c', 0.12)},
-    0 1px 2px ${rgba('#090a0c', 0.24)};
-  color: #f3f5f6;
-  font-size: 13px;
-`;
-
-const Title = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 9px 10px 9px 8px;
-  margin-right: 10px;
-
-  background: linear-gradient(#23292f, #1a1f23);
-  color: #738596;
-  font-family: ${props => props.theme.forza};
-  font-weight: 700;
-`;
-
-const Content = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-`;
-
-const Actor = styled.div`
-  font-family: ${props => props.theme.gotham};
-  font-weight: 700;
-  flex: 1;
-`;
-
-const Prime = styled.div`
-  border-radius: 2px;
-  background: #019cdc;
-  padding: 1px 4px 0px;
-`;
-
-const Crown = styled.img`
-  width: 16px;
-`;
-
-const Length = styled.div`
-  margin-left: 6px;
-  padding: 2px 4px 0px;
-  box-shadow: 0 0 0 1px #586674;
-  border-radius: 2px;
-  font-family: ${props => props.theme.miedinger};
-
-  span {
-    font-size: 10px;
-  }
-`;
-
 const Subscriber = props => {
   const username = props.recipient ? props.recipient : props.username;
   return (
@@ -184,7 +124,7 @@ LatestSubscriber.defaultProps = defaultProps;
 Subscriber.propTypes = contentPropsTypes;
 Subscriber.defaultProps = contentDefaultProps;
 
-function mapStateToProps(state) {
+const mapStateToProps = state => {
   const subscriber = selectors.getLatestSubscription(state);
   return {
     isFetching: state.subscriptions.get('isFetchingLatestSubscriber'),
@@ -194,15 +134,78 @@ function mapStateToProps(state) {
     months: subscriber.get('months'),
     prime: subscriber.get('prime')
   };
-}
+};
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
     {
       request: () => dispatch(latestSubscriberFetch.request())
     },
     dispatch
   );
-}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 8px;
+  overflow: hidden;
+
+  background: linear-gradient(#2c333a, #23292f);
+  border-radius: 4px;
+  box-shadow: 0 1px 3px ${rgba('#090a0c', 0.12)},
+    0 1px 2px ${rgba('#090a0c', 0.24)};
+  color: #f3f5f6;
+  font-size: 13px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 9px 10px 9px 8px;
+  margin-right: 10px;
+
+  background: linear-gradient(#23292f, #1a1f23);
+  color: #738596;
+  font-family: ${props => props.theme.forza};
+  font-weight: 700;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Actor = styled.div`
+  ${ellipsis()};
+
+  font-family: ${props => props.theme.gotham};
+  font-weight: 700;
+  flex: 1;
+`;
+
+const Prime = styled.div`
+  margin-left: 4px;
+  padding: 1px 4px 0px;
+  border-radius: 2px;
+  background: #019cdc;
+`;
+
+const Crown = styled.img`
+  width: 16px;
+`;
+
+const Length = styled.div`
+  margin-left: 4px;
+  padding: 3px 5px 3px 6px;
+  box-shadow: inset 0 0 0 1px #586674;
+  border-radius: 2px;
+  font-family: ${props => props.theme.miedinger};
+
+  span {
+    font-size: 10px;
+  }
+`;
 
 export default connect(mapStateToProps, mapDispatchToProps)(LatestSubscriber);
