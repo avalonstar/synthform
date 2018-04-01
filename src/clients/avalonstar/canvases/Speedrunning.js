781b26f3f0c1983d44b01993900e06d7f98ffd16
Bryan Veloso <bryan@avalonstar.com>
2018-04-01 02:06:46 -0700

    New Speedrunning layout (with cam).

--- src/clients/avalonstar/canvases/Speedrunning.js
+++ src/clients/avalonstar/canvases/Speedrunning.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 
 import styled from 'styled-components';
-import { rgba } from 'polished';
 import { Frame } from 'clients/avalonstar/styles';
 
 import {
+  ActivityCamera,
+  Generic,
   Panel,
   Notifier,
   SubPoints,
@@ -18,23 +19,22 @@ const propTypes = {};
 const Layout = ({ user }) => (
   <Container>
     <Console />
-    <Aside>
-      {/* <StyledCamera flipped /> */}
-      <Providers.Events user={user}>
-        {props => (
-          <Fragment>
-            <StyledNotifier notifierPool={props.notifierPool} />
-            <Panel events={props.payload} />
-          </Fragment>
-        )}
-      </Providers.Events>
-      <Providers.SubPoints user={user}>
-        {props => <StyledSubPoints points={props.payload} />}
-      </Providers.SubPoints>
-      <Providers.Uptime user={user}>
-        {props => <StyledUptime title="!uptime" startTime={props.payload} />}
-      </Providers.Uptime>
-    </Aside>
+    <TimerPlaceholder />
+    <StyledCamera />
+    <Providers.Events user={user}>
+      {props => (
+        <Fragment>
+          <StyledNotifier notifierPool={props.notifierPool} />
+          <StyledPanel events={props.payload} />
+        </Fragment>
+      )}
+    </Providers.Events>
+    <Providers.SubPoints user={user}>
+      {props => <StyledSubPoints points={props.payload} />}
+    </Providers.SubPoints>
+    <Providers.Uptime user={user}>
+      {props => <StyledUptime title="!uptime" startTime={props.payload} />}
+    </Providers.Uptime>
   </Container>
 );
 /* eslint-enable react/prop-types */
@@ -47,26 +47,48 @@ const Speedrunning = () => (
 
 Speedrunning.propTypes = propTypes;
 
-// const StyledCamera = styled(ActivityCamera)`
-//   grid-column: 2 / span 4;
-//   grid-row: 1;
-//   align-self: start;
-//   margin-top: -24px;
-//   z-index: 400;
-// `;
+const StyledCamera = styled(ActivityCamera)`
+  grid-column: 2 / span 5;
+  grid-row: 12;
+  align-self: end;
+  margin-top: -24px;
+  z-index: 400;
+`;
 
-const StyledNotifier = styled(Notifier)``;
+const StyledPanel = styled(Panel)`
+  grid-column: 2 / span 5;
+  grid-row: 9;
+  align-self: center;
+`;
+
+const StyledNotifier = styled(Notifier)`
+  grid-column: 2 / span 5;
+  grid-row: 11;
+  align-self: end;
+  z-index: 300;
+
+  &[data-event='follow'] {
+    align-self: start;
+    z-index: 0;
+  }
+`;
 
 const StyledSubPoints = styled(SubPoints)`
-  grid-column: 2;
+  grid-column: 2 / span 5;
+  grid-row: 8;
+  align-self: end;
+`;
+
+const StyledUptime = styled(Uptime)`
+  display: none;
+  grid-column: 3 / span 3;
   grid-row: 12;
   align-self: center;
 `;
 
-const StyledUptime = styled(Uptime)`
-  grid-column: 3 / span 3;
-  grid-row: 12;
-  align-self: center;
+const TimerPlaceholder = styled(Generic)`
+  grid-column: 2 / span 5;
+  grid-row: 1 / span 7;
 `;
 
 const Container = styled.div`
@@ -74,49 +96,19 @@ const Container = styled.div`
   align-self: end;
 
   display: grid;
-  grid-template-columns: repeat(17, 80px);
+  grid-template-columns: ${1200 - 36}px repeat(5, 65.5px);
   grid-template-rows: repeat(12, 62px);
   grid-gap: 12px;
-`;
-
-const Aside = styled.aside`
-  grid-column: 13 / span 5;
-  grid-row: 1 / span 12;
-  margin: 0 -24px 0 -12px;
-
-  position: relative;
-  display: grid;
-  grid-template-columns: 12px 1fr 1fr 1fr 1fr 12px;
-  grid-template-rows: repeat(12, 62px);
-  grid-gap: 12px;
-
-  &::before,
-  &::after {
-    content: '';
-    position: absolute;
-    top: -24px;
-    width: 8px;
-    height: calc(100% + 24px);
-
-    background-repeat: repeat-y;
-    z-index: 200;
-  }
-  &::before {
-    background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAACwCAYAAAAyli+TAAAArElEQVRYhe3VMQqDQBSE4d/gNaytPYIHECtBG/VoJk0CqYIH8GimCEpQcQNL0F3GSuRjeDjL2yCKkzHNKgDaogSgez4AGPo7FwyPgICAryCI4mTcAyHA3gKxTzjDf3ACqCyfQLj80NS5OSHNqrmfVQJ8SrJLmGr/qe7NhOnA/HHI74QVuN5e83tT5+Yhz3AenABaQQ4BleUT0H1hM6Tui0OAVpBDQGUJCAgcCN43oZAKzq+1fqaaaabjru5erkjggg==);
-    box-shadow: 1px 0 10px ${rgba('#090a0c', 0.5)};
-  }
-  &::after {
-    right: 0;
-    background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAACwCAYAAAAyli+TAAAAr0lEQVRYhe3VMQ6CQBCF4R/DNaipOQIHIFYm0qhHUxpMrAwH4GhYgQXLQKIGxrwtyZe3w85mNkrSrMuLEoDL4QjA9XEHoG1qdswsAQGBfwVRkmadBWIAa4B8nrCFc3AB1CxHIIb3CU6C82k/fLhVz/EWeVEO/QgmtE1tFzmb0BcZSlr2m/0F+WGRVkLw0o62sJbAQqAR5AioWY6A3ouvFqn3YhNAI8gRULMEBARWBC+ATpAOu8sdVQAAAABJRU5ErkJggg==);
-    box-shadow: -1px 0 10px ${rgba('#090a0c', 0.5)};
-  }
 `;
 
 const Console = styled.div`
-  background: #000;
-  grid-column: 1 / span 12;
-  grid-row: 1 / span 12;
+  display: block;
+  position: absolute;
+  width: 1200px;
+  height: 900px;
+  padding-top: ${3 / 4 * 100}%;
 
+  grid-column: 1 / span 12;
   margin: -24px 0 0 -24px;
 `;
 
