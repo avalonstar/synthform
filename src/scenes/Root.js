991157b7999a3b8c0d6f88d5755e78b9d23607dc
Bryan Veloso <bryan@avalonstar.com>
2018-06-28 20:49:49 -0700

    Connect react-firestore to the Root component.

--- src/scenes/Root.js
+++ src/scenes/Root.js
@@ -3,6 +3,10 @@ import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
+import firebase from '@firebase/app';
+import '@firebase/firestore';
+import { FirestoreProvider } from 'react-firestore';
+
 import { ThemeProvider } from 'styled-components';
 
 import App from 'scenes/App';
@@ -15,6 +19,18 @@ import {
 import { Madness, Whammy } from 'clients/special/canvases';
 import { foundation } from 'helpers/foundation';
 import { history } from 'store';
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_URI
+} = process.env;
+
+firebase.initializeApp({
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: REACT_APP_FIREBASE_URI
+});
 
 const Main = () => (
   <Switch>
@@ -34,13 +50,15 @@ const Main = () => (
 );
 
 const Root = ({ store }) => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <ThemeProvider theme={foundation}>
-        <Main />
-      </ThemeProvider>
-    </ConnectedRouter>
-  </Provider>
+  <FirestoreProvider firebase={firebase}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <ThemeProvider theme={foundation}>
+          <Main />
+        </ThemeProvider>
+      </ConnectedRouter>
+    </Provider>
+  </FirestoreProvider>
 );
 
 export default Root;
